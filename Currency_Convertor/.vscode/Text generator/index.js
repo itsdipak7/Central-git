aa1e5c23e07f21b707182760fe9763cd9624aad2
itsdipak7 <123625499+itsdipak7@users.noreply.github.com>
2023-02-02 15:38:13 +0530

    Delete Currency_Convertor directory

--- Currency_Convertor/.vscode/Text generator/index.js
+++
@@ -1,7 +0,0 @@
-const generateBtn = document.getElementById("generateBtn");
-const inputText = document.getElementById("inputText");
-const generatedText = document.getElementById("generatedText");
-
-generateBtn.addEventListener("click", function() {
-    generatedText.textContent = inputText.value + " - generated text";
-});
